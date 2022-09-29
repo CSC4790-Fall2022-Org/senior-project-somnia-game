9c5efb58dd0a75031cf6248b7e542bcb01980184
jsullivan1201 <jsullivan1201@gmail.com>
2022-09-29 15:11:30 -0400

    added clock assets

---
+++ Assets/Scripts/timepassage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class timepassage : MonoBehaviour
+{
+    DateTime time;
+    int hour;
+    int minute;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int hour = 8;
+        int minute = 0;
+        time = new DateTime(1999,1,1,hour,minute,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time.Hour != hour){
+            time.Hour = hour;
+        }
+        clock.sprite = Resources.Load<Sprite>("clock" + time.Hour);
+
+    }
+}
