f7f1f53e01e4ae69bb022e7e9cf7832dd2172875
Juliana Goicoechea <72264833+jgoicoec@users.noreply.github.com>
2022-10-04 16:31:26 -0400

    Add Quit Manager and Quit function

---
+++ Assets/Scripts/QuitManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class QuitManager : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    
+}
