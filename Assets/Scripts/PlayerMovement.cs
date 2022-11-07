df9a4376ce039adfc714d75edc5456a5a1b88b2a
lrosari1 <lrosari1@villanova.edu>
2022-11-07 13:43:20 -0500

    Shift Player Position (not done)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movement = Vector2.zero;
     public KeyCode shiftLeft;
     public KeyCode shiftRight;
+    public Transform player;
+    public Transform pos1,pos2,pos3,pos4,pos5;
     int lane = 3;
     Vector2 playerPosition = new Vector2(-2, -3);
     bool movementBool = true;
@@ -30,8 +32,26 @@ public class PlayerMovement : MonoBehaviour
     void checkInput()
     {
         if(Input.GetKeyDown(shiftLeft) && movementBool && lane > 1){
-            lane --;
-            transform.position = new Vector2(-1,-2);
+            if(lane == 3){
+            player.position = pos2.position;
+            lane--;
+            }
+
+            if(lane == 2){
+            player.position = pos1.position;
+            lane--;
+            }
+
+            if(lane == 4){
+            player.position = pos3.position;
+            lane--;
+            }
+
+            if(lane == 5){
+            player.position = pos4.position;
+            lane--;
+            }
+
         }
         if(Input.GetKeyDown(shiftRight) && movementBool && lane < 5){
             lane ++;
