ffde765d73af4e328db640a8371ab2b578985a1b
jsullivan1201 <jsullivan1201@gmail.com>
2022-11-11 14:24:52 -0500

    Added quicktime result popup

---
+++ Assets/Scripts/QuicktimeResult.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuicktimeResult : MonoBehaviour
+{
+    public TextMeshProUGUI enText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void setResult(string result){
+        enText.SetText(result);
+    }
+}
