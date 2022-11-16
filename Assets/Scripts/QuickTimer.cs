e98f45a5bd81e067e5e233fe956e1c7fc8dd58b6
Lucy Wang <43558154+gujiguj@users.noreply.github.com>
2022-11-16 01:12:44 -0500

    Add pause for default music on qte

--- Assets/Scripts/QuickTimer.cs
+++ Assets/Scripts/QuickTimer.cs
@@ -13,17 +13,30 @@ public class QuickTimer : MonoBehaviour
     QuicktimeResult r1;
     GameObject overlay;
     DefineStats stats;
+    AudioSource defaultMusic;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        defaultMusic = GameObject.FindGameObjectWithTag("DefaultMusic").GetComponent<AudioSource>();
+        if (defaultMusic != null)
+        {
+            defaultMusic.Pause();
+        }
         timeRemaining = 10;
         timerText.SetText("0:10");
         result = GameObject.Find("QuickTimeResult");
         overlay = GameObject.Find("UIOverlay");
         r1 = result.GetComponent<QuicktimeResult>();
         stats = overlay.GetComponent<DefineStats>();
+    }
+
+    public void PlayDefaultMusic()
+    {
+        if (defaultMusic != null && !defaultMusic.isPlaying)
+        {
+            defaultMusic.UnPause();
+        }
     }
 
     // Update is called once per frame
@@ -39,7 +52,7 @@ public class QuickTimer : MonoBehaviour
         }
         else{
             r1.SetActive();
-            r1.setResult("You embarass yourself in front of your peers. Your Stress increased and your happiness decreased!");
+            r1.setResult("You embarrass yourself in front of your peers. Your Stress increased and your happiness decreased!");
             stats.changeHappiness(-10);
             stats.changeStress(20);
             gameObject.SetActive(false);
