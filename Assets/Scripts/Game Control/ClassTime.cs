6f9e99c270c1645fc4d7354656e753f3bab8d034
jsullivan1201 <jsullivan1201@gmail.com>
2022-12-05 13:22:47 -0500

    fixed take notes button hover

--- Assets/Scripts/Game Control/ClassTime.cs
+++ Assets/Scripts/Game Control/ClassTime.cs
@@ -2,27 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ClassTime : MonoBehaviour
 {
     Button b1;
     GameObject b;
+    TextMeshProUGUI b2;
     void Start()
     {
         
         b = GameObject.Find("LearnButton");
         b1 = b.GetComponent<Button>();
-        
+        b2 = b.GetComponent<TextMeshProUGUI>();
 
         //Debug.Log(Globals.hour);
         if(Globals.hour == 21){
             if(Globals.minute <=1){
                b1.interactable = true;
+               b2.SetText(" ");
             }
         }
         else if(Globals.hour == 20){
             if(Globals.minute >=30){
                 b1.interactable = true;
+                b2.SetText(" ");
             }
         }
     }
